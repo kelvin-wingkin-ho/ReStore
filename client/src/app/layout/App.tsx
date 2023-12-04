96e9a32b44e12ca172db7cbe4ea25fc1290280b0
Kelvin Ho <kelvin.wingkin.ho@gmail.com>
2023-12-04 16:36:17 +1100

    End of section 4

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -3,6 +3,7 @@ import { Container, CssBaseline, createTheme } from "@mui/material";
 import Header from "./Header";
 import { ThemeProvider } from "@emotion/react";
 import { useState } from "react";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -25,7 +26,7 @@ function App() {
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
       <Container>
-        <Catalog/>
+        <Outlet/>
       </Container>
     </ThemeProvider>
   )
